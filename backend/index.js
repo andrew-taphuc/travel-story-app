39ac6bd7f330796759825877a5453c29bc3eb257
Ta Hong Phuc <taphuc1@gmail.com>
2024-11-13 10:25:01 +0700

    default

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
-
 const config = require("./config.json");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-// const upload = require("./multer");
+const upload = require("./multer");
 const fs = require("fs");
 const path = require("path");
 
@@ -120,183 +119,183 @@ app.get("/get-user", authenticateToken, async (req, res) => {
   });
 });
 
-// // Route to handle image upload
-// app.post("/image-upload", upload.single("image"), async (req, res) => {
-//   try {
-//     if (!req.file) {
-//       return res
-//         .status(400)
-//         .json({ error: true, message: "No image uploaded" });
-//     }
+// Route to handle image upload
+app.post("/image-upload", upload.single("image"), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res
+        .status(400)
+        .json({ error: true, message: "No image uploaded" });
+    }
 
-//     const imageUrl = `http://localhost:8000/uploads/${req.file.filename}`;
+    const imageUrl = `http://localhost:8000/uploads/${req.file.filename}`;
 
-//     res.status(200).json({ imageUrl });
-//   } catch (error) {
-//     res.status(500).json({ error: true, message: error.message });
-//   }
-// });
+    res.status(200).json({ imageUrl });
+  } catch (error) {
+    res.status(500).json({ error: true, message: error.message });
+  }
+});
 
-// // Delete an image from uploads folder
-// app.delete("/delete-image", async (req, res) => {
-//   const { imageUrl } = req.query;
+// Delete an image from uploads folder
+app.delete("/delete-image", async (req, res) => {
+  const { imageUrl } = req.query;
 
-//   if (!imageUrl) {
-//     return res
-//       .status(400)
-//       .json({ error: true, message: "imageUrl parameter is required" });
-//   }
+  if (!imageUrl) {
+    return res
+      .status(400)
+      .json({ error: true, message: "imageUrl parameter is required" });
+  }
 
-//   try {
-//     // Extract the filename from the imageUrl
-//     const filename = path.basename(imageUrl);
+  try {
+    // Extract the filename from the imageUrl
+    const filename = path.basename(imageUrl);
 
-//     // Define the file path
-//     const filePath = path.join(__dirname, "uploads", filename);
+    // Define the file path
+    const filePath = path.join(__dirname, "uploads", filename);
 
-//     // Check if the file exists
-//     if (fs.existsSync(filePath)) {
-//       // Delete the file from the uploads folder
-//       fs.unlinkSync(filePath);
-//       res.status(200).json({ message: "Image deleted successfully" });
-//     } else {
-//       res.status(200).json({ error: true, message: "Image not found" });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ error: true, message: error.message });
-//   }
-// });
+    // Check if the file exists
+    if (fs.existsSync(filePath)) {
+      // Delete the file from the uploads folder
+      fs.unlinkSync(filePath);
+      res.status(200).json({ message: "Image deleted successfully" });
+    } else {
+      res.status(200).json({ error: true, message: "Image not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: true, message: error.message });
+  }
+});
 
-// // Serve static files from the uploads and assets directory
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-// app.use("/assets", express.static(path.join(__dirname, "assets")));
+// Serve static files from the uploads and assets directory
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
-// // Add Travel Story
-// app.post("/add-travel-story", authenticateToken, async (req, res) => {
-//   const { title, story, visitedLocation, imageUrl, visitedDate } = req.body;
-//   const { userId } = req.user;
+// Add Travel Story
+app.post("/add-travel-story", authenticateToken, async (req, res) => {
+  const { title, story, visitedLocation, imageUrl, visitedDate } = req.body;
+  const { userId } = req.user;
 
-//   // Validate required fields
-//   if (!title || !story || !visitedLocation || !imageUrl || !visitedDate) {
-//     return res
-//       .status(400)
-//       .json({ error: true, message: "All fields are required" });
-//   }
+  // Validate required fields
+  if (!title || !story || !visitedLocation || !imageUrl || !visitedDate) {
+    return res
+      .status(400)
+      .json({ error: true, message: "All fields are required" });
+  }
 
-//   // Convert visitedDate from milliseconds to Date object
-//   const parsedVisitedDate = new Date(parseInt(visitedDate));
+  // Convert visitedDate from milliseconds to Date object
+  const parsedVisitedDate = new Date(parseInt(visitedDate));
 
-//   try {
-//     const travelStory = new TravelStory({
-//       title,
-//       story,
-//       visitedLocation,
-//       userId,
-//       imageUrl,
-//       visitedDate: parsedVisitedDate,
-//     });
+  try {
+    const travelStory = new TravelStory({
+      title,
+      story,
+      visitedLocation,
+      userId,
+      imageUrl,
+      visitedDate: parsedVisitedDate,
+    });
 
-//     await travelStory.save();
-//     res.status(201).json({ story: travelStory, message: "Added Successfully" });
-//   } catch (error) {
-//     res.status(400).json({ error: true, message: error.message });
-//   }
-// });
+    await travelStory.save();
+    res.status(201).json({ story: travelStory, message: "Added Successfully" });
+  } catch (error) {
+    res.status(400).json({ error: true, message: error.message });
+  }
+});
 
-// // Get All Travel Stories
-// app.get("/get-all-stories", authenticateToken, async (req, res) => {
-//   const { userId } = req.user;
+// Get All Travel Stories
+app.get("/get-all-stories", authenticateToken, async (req, res) => {
+  const { userId } = req.user;
 
-//   try {
-//     const travelStories = await TravelStory.find({ userId: userId }).sort({
-//       isFavourite: -1,
-//     });
-//     res.status(200).json({ stories: travelStories });
-//   } catch (error) {
-//     res.status(500).json({ error: true, message: error.message });
-//   }
-// });
+  try {
+    const travelStories = await TravelStory.find({ userId: userId }).sort({
+      isFavourite: -1,
+    });
+    res.status(200).json({ stories: travelStories });
+  } catch (error) {
+    res.status(500).json({ error: true, message: error.message });
+  }
+});
 
-// // Edit Travel Story
-// app.put("/edit-story/:id", authenticateToken, async (req, res) => {
-//   const { id } = req.params;
-//   const { title, story, visitedLocation, imageUrl, visitedDate } = req.body;
-//   const { userId } = req.user;
+// Edit Travel Story
+app.put("/edit-story/:id", authenticateToken, async (req, res) => {
+  const { id } = req.params;
+  const { title, story, visitedLocation, imageUrl, visitedDate } = req.body;
+  const { userId } = req.user;
 
-//   // Validate required fields
-//   if (!title || !story || !visitedLocation || !visitedDate) {
-//     return res
-//       .status(400)
-//       .json({ error: true, message: "All fields are required" });
-//   }
+  // Validate required fields
+  if (!title || !story || !visitedLocation || !visitedDate) {
+    return res
+      .status(400)
+      .json({ error: true, message: "All fields are required" });
+  }
 
-//   // Convert visitedDate from milliseconds to Date object
-//   const parsedVisitedDate = new Date(parseInt(visitedDate));
+  // Convert visitedDate from milliseconds to Date object
+  const parsedVisitedDate = new Date(parseInt(visitedDate));
 
-//   try {
-//     // Find the travel story by ID and ensure it belongs to the authenticated user
-//     const travelStory = await TravelStory.findOne({ _id: id, userId: userId });
+  try {
+    // Find the travel story by ID and ensure it belongs to the authenticated user
+    const travelStory = await TravelStory.findOne({ _id: id, userId: userId });
 
-//     if (!travelStory) {
-//       return res
-//         .status(404)
-//         .json({ error: true, message: "Travel story not found" });
-//     }
+    if (!travelStory) {
+      return res
+        .status(404)
+        .json({ error: true, message: "Travel story not found" });
+    }
 
-//     const placeholderImgUrl = `http://localhost:8000/assets/placeholder.png`;
+    const placeholderImgUrl = `http://localhost:8000/assets/placeholder.png`;
 
-//     travelStory.title = title;
-//     travelStory.story = story;
-//     travelStory.visitedLocation = visitedLocation;
-//     travelStory.imageUrl = imageUrl || placeholderImgUrl;
-//     travelStory.visitedDate = parsedVisitedDate;
+    travelStory.title = title;
+    travelStory.story = story;
+    travelStory.visitedLocation = visitedLocation;
+    travelStory.imageUrl = imageUrl || placeholderImgUrl;
+    travelStory.visitedDate = parsedVisitedDate;
 
-//     await travelStory.save();
-//     res.status(200).json({ story: travelStory, message: "Update Successful" });
-//   } catch (error) {
-//     res.status(500).json({ error: true, message: error.message });
-//   }
-// });
+    await travelStory.save();
+    res.status(200).json({ story: travelStory, message: "Update Successful" });
+  } catch (error) {
+    res.status(500).json({ error: true, message: error.message });
+  }
+});
 
-// // Delete a travel story
-// app.delete("/delete-story/:id", authenticateToken, async (req, res) => {
-//   const { id } = req.params;
-//   const { userId } = req.user;
+// Delete a travel story
+app.delete("/delete-story/:id", authenticateToken, async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.user;
 
-//   try {
-//     // Find the travel story by ID and ensure it belongs to the authenticated user
-//     const travelStory = await TravelStory.findOne({ _id: id, userId: userId });
+  try {
+    // Find the travel story by ID and ensure it belongs to the authenticated user
+    const travelStory = await TravelStory.findOne({ _id: id, userId: userId });
 
-//     if (!travelStory) {
-//       return res
-//         .status(404)
-//         .json({ error: true, message: "Travel story not found" });
-//     }
+    if (!travelStory) {
+      return res
+        .status(404)
+        .json({ error: true, message: "Travel story not found" });
+    }
 
-//     // Delete the travel story from the database
-//     await travelStory.deleteOne({ _id: id, userId: userId });
+    // Delete the travel story from the database
+    await travelStory.deleteOne({ _id: id, userId: userId });
 
-//     // Extract the filename from the imageUrl
-//     const imageUrl = travelStory.imageUrl;
-//     const filename = path.basename(imageUrl);
+    // Extract the filename from the imageUrl
+    const imageUrl = travelStory.imageUrl;
+    const filename = path.basename(imageUrl);
 
-//     // Define the file path
-//     const filePath = path.join(__dirname, "uploads", filename);
+    // Define the file path
+    const filePath = path.join(__dirname, "uploads", filename);
 
-//     // Delete the image file from the uploads folder
-//     fs.unlink(filePath, (err) => {
-//       if (err) {
-//         console.error("Failed to delete image file:", err);
-//         // Optionally, you could still respond with a success status here
-//         // if you don't want to treat this as a critical error.
-//       }
-//     });
+    // Delete the image file from the uploads folder
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error("Failed to delete image file:", err);
+        // Optionally, you could still respond with a success status here
+        // if you don't want to treat this as a critical error.
+      }
+    });
 
-//     res.status(200).json({ message: "Travel story deleted successfully" });
-//   } catch (error) {
-//     res.status(500).json({ error: true, message: error.message });
-//   }
-// });
+    res.status(200).json({ message: "Travel story deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: true, message: error.message });
+  }
+});
 
 // Update isFavourite
 app.put("/update-is-favourite/:id", authenticateToken, async (req, res) => {
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:8000'
-export const BASE_URL = 'https://travel-story-backend-seven.vercel.app/'
+export const BASE_URL = 'https://travel-story-backend-gold.vercel.app'
